feabc1a503348320c2d1e3003a4f94ba01c9648d
x88xx <78026190+x88xx@users.noreply.github.com>
2024-10-16 20:11:10 +0800

    -

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# 修改个性化信息
+# date=`date +%Y.%m.%d`
+# sed -i 's/OpenWrt/OpenWrt Build '$date' By Dream./g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/%D %V, %C/%D %V, '$date' By Dream./g' package/base-files/files/etc/banner
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+# 修改luci显示cpu型号方式
+sed -i 's/pcdata(boardinfo.model/pcdata(boardinfo.system/g' package/lean/autocore/files/x86/index.htm
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+#修改内核版本（版本内核默认6.1，版本修改为:5.1）
+# sed -i 's/KERNEL_PATCHVER:=\([0-9]\+\.[0-9]\+\)/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
 #
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
