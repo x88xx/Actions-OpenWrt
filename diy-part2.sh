ba84b569eeb2ec9891fb2f5de178afd2328e6d43
x88xx <78026190+x88xx@users.noreply.github.com>
2024-10-16 20:11:38 +0800

    -

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 创建自定义配置文件
+git clone https://github.com/x88xx/Actions-OpenWrt.git package/tmp
+cp -r package/tmp/hello-openwrt package/ && rm -rf package/tmp/
