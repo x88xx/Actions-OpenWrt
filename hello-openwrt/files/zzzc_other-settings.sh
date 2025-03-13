534d2931f0ea1b234b24e7e67a89391fa29c9c15
x88xx <78026190+x88xx@users.noreply.github.com>
2025-03-13 12:29:14 +0800

    -

--- hello-openwrt/files/zzzc_other-settings.sh
+++ hello-openwrt/files/zzzc_other-settings.sh
@@ -9,9 +9,6 @@
 # 修改默认主题
 # sed -i "s|option mediaurlbase '/luci-static/bootstrap'|option mediaurlbase '/luci-static/bootstrap-light'|g" /etc/config/luci
 
-# 30分钟后，自动关闭HTTP 和 SSH 功能。
-(echo "@reboot (sleep 1800 && /etc/init.d/dropbear stop && /etc/init.d/uhttpd stop) &" | crontab -)
-
 # 删除无用文件
 rm -rf /root/*
 rm -rf /etc/config/ddns
