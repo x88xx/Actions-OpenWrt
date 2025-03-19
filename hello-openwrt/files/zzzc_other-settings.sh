a58648153e6eb1b3589f1dcf1ecda82273ad09fd
x88xx <78026190+x88xx@users.noreply.github.com>
2025-03-19 18:15:55 +0800

    -

--- hello-openwrt/files/zzzc_other-settings.sh
+++ hello-openwrt/files/zzzc_other-settings.sh
@@ -2,9 +2,7 @@
 
 # 设置系统名
 uci set system.@system[0].hostname='OpenWRT'
-# uci set system.@system[-1].hostname='OpenWRT'
-# set system.@system[0].hostname='OpenWRT'
-# set system.@system[-1].hostname='OpenWRT'
+
 
 # 修改默认主题
 # sed -i "s|option mediaurlbase '/luci-static/bootstrap'|option mediaurlbase '/luci-static/bootstrap-light'|g" /etc/config/luci
