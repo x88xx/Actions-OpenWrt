1588dab35c8a952de68a9225d3d655e36cc36378
x88xx <78026190+x88xx@users.noreply.github.com>
2025-05-17 20:45:36 +0800

    -

--- hello-openwrt/other/create_swap.sh
+++ hello-openwrt/other/create_swap.sh
@@ -2,14 +2,14 @@
 
 create_swap() {
     local swap_file="/www/swapfile"
-    local swap_size="128M"
+    local swap_size="128"
 
     # 确保/www目录存在
     mkdir -p /www
 
     # 如果swap文件不存在，则创建它
     if [ ! -f "$swap_file" ]; then
-        dd if=/dev/zero of="$swap_file" bs=1M count=128
+        dd if=/dev/zero of="$swap_file" bs=1M count=swap_size
         chmod 0755 "$swap_file"
         mkswap "$swap_file"
     fi
