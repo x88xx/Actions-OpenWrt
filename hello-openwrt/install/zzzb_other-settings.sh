ec2b57aeb72b6ce5e90a0bc6228be8bca7163c8f
x88xx <78026190+x88xx@users.noreply.github.com>
2025-05-17 21:31:57 +0800

    -

--- hello-openwrt/install/zzzb_other-settings.sh
+++ hello-openwrt/install/zzzb_other-settings.sh
@@ -6,6 +6,9 @@ uci set system.@system[0].hostname='OpenWRT'
 # 修改默认主题
 # sed -i "s|option mediaurlbase '/luci-static/bootstrap'|option mediaurlbase '/luci-static/bootstrap-light'|g" /etc/config/luci
 
+# 添加SWAP
+/root/other/create_swap.sh
+
 # 删除无用文件
 rm -rf /root/*
 rm -rf /etc/uhttpd*
