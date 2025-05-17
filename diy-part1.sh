c3d96084936ea5fa6f84abf8fa954c9103f945bc
x88xx <78026190+x88xx@users.noreply.github.com>
2025-05-17 16:48:55 +0800

    -

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # sed -i 's/pcdata(boardinfo.model/pcdata(boardinfo.system/g' package/lean/autocore/files/x86/index.htm
 #
 # 修改内核版本（版本内核默认6.1，版本修改为:5.4）
-# sed -i 's/KERNEL_PATCHVER:=\([0-9]\+\.[0-9]\+\)/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=\([0-9]\+\.[0-9]\+\)/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
 #
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
@@ -18,5 +18,3 @@
 # 有些时候差python环境
 # sudo apt-get update && sudo apt-get install python3-venv
 #
-# 替换源
-sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
