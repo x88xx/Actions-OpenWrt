0ead9f913de087496f9de72c9cbfcf67d1cb34bc
x88xx <78026190+x88xx@users.noreply.github.com>
2025-05-17 23:28:27 +0800

    Add files via upload

---
+++ hello-openwrt/soft/create_swap.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+create_swap() {
+
+    # 创建路径
+    local swap_file="/www/swapfile"
+
+    # 128M
+    local swap_size="128"
+
+    # 确保/www目录存在
+    mkdir -p /www
+
+    # 如果swap文件不存在，则创建它
+    if [ ! -f "$swap_file" ]; then
+        dd if=/dev/zero of="$swap_file" bs=1M count=swap_size
+        chmod 0755 "$swap_file"
+        mkswap "$swap_file"
+    fi
+}
+
+# 调用函数
+create_swap
+
+exit 0
