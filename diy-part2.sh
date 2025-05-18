73b3e827ef5a93bb4b2ee7b881ba7667538e26b3
x88xx <78026190+x88xx@users.noreply.github.com>
2025-05-18 15:10:41 +0800

    -

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# 创建自定义配置文件
-# mkdir -p package/tmp
-# git clone https://github.com/x88xx/Actions-OpenWrt.git package/tmp
-# cp -r package/tmp/hello-openwrt package/ && rm -rf package/tmp/
+
+exit 0
